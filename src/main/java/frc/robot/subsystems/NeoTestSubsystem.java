260d8360800ea08dcff30955767fd644db68a120
Scott <scottwork321@gmail.com>
2020-01-11 11:37:44 -0500

    Added Voltage and Current methods to NeoTestSubsystem

--- src/main/java/frc/robot/subsystems/NeoTestSubsystem.java
+++ src/main/java/frc/robot/subsystems/NeoTestSubsystem.java
@@ -4,25 +4,39 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpiutil.math.MathUtil;
 
-public class NeoTestSubsystem extends SubsystemBase{
+public class NeoTestSubsystem extends SubsystemBase {
 
     private static final int deviceID = 1;
+    private static final double MAX_NEO_VOLTAGE = 12;
 
-    private CANSparkMax testMotor = new CANSparkMax(deviceID, MotorType.kBrushless);
+    private CANSparkMax testMotor;
 
-    public void setCurrentLimit(int currentLimit){
+    public NeoTestSubsystem() {
+        testMotor = new CANSparkMax(deviceID, MotorType.kBrushless);
+    }
+
+    public void setCurrentLimit(int currentLimit) {
         testMotor.setSmartCurrentLimit(currentLimit);
         testMotor.setSecondaryCurrentLimit(currentLimit);
     }
 
-    public void setPower(double power){
+    public void setPower(double power) {
         testMotor.set(power);
     }
 
-    public double getCurrent(){
+    public double getCurrent() {
         return testMotor.getOutputCurrent();
-        testMotor.setVoltage(outputVolts);
+    }
+
+    public void setVoltage(double voltage) {
+        MathUtil.clamp(voltage, -MAX_NEO_VOLTAGE, MAX_NEO_VOLTAGE);
+        testMotor.setVoltage(voltage);
+    }
+
+    public double getOutputCurrent() {
+        return testMotor.getOutputCurrent();
     }
 
 }
