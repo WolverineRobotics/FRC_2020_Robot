70fa77659c9b054ed83376f3242000a5d4422f2f
ImJeremyy <madivoso@gmail.com>
2020-01-28 21:49:34 -0500

    Renamed 'NtEntryNullException.java' to 'NTNullEntryException.java'

--- src/main/java/frc/robot/exceptions/NTNullEntryException.java
+++ src/main/java/frc/robot/exceptions/NTNullEntryException.java
@@ -3,11 +3,8 @@ package frc.robot.exceptions;
 /**
  * Thown when getting an entry from NetworkTables returns the default value.
  */
-public class NtEntryNullException extends Exception {
-    public NtEntryNullException() {
-    }
-
-    public NtEntryNullException(String message) {
+public class NTEntryNullException extends Exception {
+    public NTEntryNullException(String message) {
         super(message);
     }
 }
