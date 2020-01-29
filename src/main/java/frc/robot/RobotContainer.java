995413926e9c91d10bd25bf9f3355d353a17e639
ImJeremyy <madivoso@gmail.com>
2020-01-28 22:52:22 -0500

    Renamed variables to convention. Read more here
    For subsystems, use "s_"
    For commands use "c_"
    For default commands use "dc_"

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,45 +17,32 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
- * This class is where the bulk of the robot should be declared.  Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
- * (including subsystems, commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  /* 
-  The default naming scheme uses the m_ prefix for both subsystems and commands.
-  We will be using the m_ prefix for subsystems and c_ prefix for default commands.
-  The provided example has been left unchanged.
-  */
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  // all subsystem instances
+  private final DriveSubsystem s_drive;
 
-  private final DriveSubsystem m_drive = new DriveSubsystem();
-  private final DefaultDriveCommand c_drive = new DefaultDriveCommand(m_drive);
+  // default commands
+  private final DefaultDriveCommand c_drive;
+
+  // autonomous command
 
   /**
-   * The container for the robot.  Contains subsystems, OI devices, and commands.
+   * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
 
-    CommandScheduler.getInstance().setDefaultCommand(m_drive, c_drive);
+    s_drive = new DriveSubsystem();
+    c_drive = new DefaultDriveCommand(s_drive);
+    CommandScheduler.getInstance().setDefaultCommand(s_drive, c_drive);
 
-    // Configure the button bindings
-    configureButtonBindings();
   }
-
-  /**
-   * Use this method to define your button->command mappings.  Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
-   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {
-  }
-
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
