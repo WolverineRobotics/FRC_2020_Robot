1496ec6ff6b5492078ee27ba11f5e11f1e204164
Scott <scottwork321@gmail.com>
2020-01-31 09:25:31 -0500

    Util methods now use library implementation
     - Most methods are left there for compatibility and convenience

--- src/main/java/frc/robot/util/Util.java
+++ src/main/java/frc/robot/util/Util.java
@@ -1,41 +1,35 @@
 package frc.robot.util;
 
-public class Util{
+import edu.wpi.first.wpiutil.math.MathUtil;
 
-    //Allows for input values less than trigger values. 
-    //Piecewise function, if less than deadzone value, 0        
-    //Else linear
-    //https://www.desmos.com/calculator/h7gnlk21iz 
+public class Util {
 
-    public static double getDeadzoneResult(double controlInput, double deadzoneAmount){
+    // Allows for input values less than trigger values.
+    // Piecewise function, if less than deadzone value, 0
+    // Else linear
+    // https://www.desmos.com/calculator/h7gnlk21iz
 
-        if(Math.abs(controlInput) < deadzoneAmount){
+    public static double getDeadzoneResult(double controlInput, double deadzoneAmount) {
+
+        if (Math.abs(controlInput) < deadzoneAmount) {
             controlInput = 0;
         } else {
-            if(controlInput>0){
-                controlInput = ((controlInput - deadzoneAmount)/(1-deadzoneAmount));
-            }else{
-                controlInput = ((controlInput + deadzoneAmount)/(1-deadzoneAmount));
-            }  
+            if (controlInput > 0) {
+                controlInput = ((controlInput - deadzoneAmount) / (1 - deadzoneAmount));
+            } else {
+                controlInput = ((controlInput + deadzoneAmount) / (1 - deadzoneAmount));
+            }
         }
         return controlInput;
     }
 
-
-    //Ensures motor power values are between -1 and 1
-    public static double getMotorLimits(double motorPower){
-        if (motorPower < -1){
-            motorPower = -1;
-        }else if(motorPower > 1){
-            motorPower = 1;
-        }
-
-        return motorPower;
+    // Ensures motor power values are between -1 and 1
+    public static double getMotorLimits(double motorPower) {
+        return MathUtil.clamp(motorPower, -1, 1);
     }
 
+    public static double setDeadzoneLimits(double controlInput, double deadzoneValue) {
 
-   public static double setDeadzoneLimits(double controlInput, double deadzoneValue){
-        
         controlInput = getDeadzoneResult(controlInput, deadzoneValue);
         controlInput = getMotorLimits(controlInput);
         return controlInput;
@@ -45,17 +39,12 @@ public class Util{
     // Ensures that a double is between 0 and 1
     // If less than 0, will be set to 0
     // If greater than 1, will be set to one
-    public static double zeroToOne(double num){
-        if(num < 0){
-            num = 0;
-        }else if (num > 1){
-            num = 1;
-        }
-        return num;
+    public static double zeroToOne(double num) {
+        return MathUtil.clamp(num, 0, 1);
     }
 
-    public static double feetToMeter(double feet){
-        return feet * 0.3048;
+    public static double toVoltage(double speed, double maxVoltage) {
+        return (speed * maxVoltage);
     }
 
 }
