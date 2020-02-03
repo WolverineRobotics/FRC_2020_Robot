88be4e65c7dc8c5395b96a36270c326a4a267c89
Austin <austinx88@gmail.com>
2020-02-03 17:56:22 -0500

    Added RobotMap Intake fillers

--- src/main/java/frc/robot/constants/RobotMap.java
+++ src/main/java/frc/robot/constants/RobotMap.java
@@ -2,20 +2,20 @@ package frc.robot.constants;
 
 public class RobotMap{
 
-    //TODO: Change addresses
+    public class Sensors {
+        public static final int BALL_SENSOR_1 = -1;
+        public static final int BALL_SENSOR_2 = -1;
+        public static final int BALL_SENSOR_3 = -1;
+        public static final int BALL_SENSOR_4 = -1;
+        public static final int BALL_SENSOR_5 = -1;
+    }
 
-    public static final int DRIVE_LEFT_MOTOR_MASTER_ADDRESS         = 1;
-    public static final int DRIVE_LEFT_MOTOR_SLAVE_ADDRESS          = 2;
-    public static final int DRIVE_RIGHT_MOTOR_MASTER_ADDRESS        = 3;
-    public static final int DRIVE_RIGHT_MOTOR_SLAVE_ADDRESS         = 4;
-
-
-    public static final int DRIVE_LEFT_ENCODER_A                    = 6;
-    public static final int DRIVE_LEFT_ENCODER_B                    = 7;
-
-    public static final int DRIVE_RIGHT_ENCODER_A                   = 4;
-    public static final int DRIVE_RIGHT_ENCODER_B                   = 5;
-
-    public static final int DRIVE_PIGEON_IMU_ADDRESS                = 12;
+    public class SpeedController {
+        public static final int ENTRY = -1;
+        public static final int CURVE = -1;
+        public static final int VERTICAL = -1;
+        public static final int SHOOT = -1;
+        public static final int HOOD = -1;
+    }
 
 }
