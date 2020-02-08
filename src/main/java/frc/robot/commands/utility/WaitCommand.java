a7cfdf92b35329b52c9a989d3abda1af6c3b8d95
Austin <austinx88@gmail.com>
2020-02-08 12:42:53 -0500

    Made a wait command
    Takes in two types of parameters: Time and subsystems.  Will wait the subsystems  passes the time set..

--- src/main/java/frc/robot/commands/utility/WaitCommand.java
+++ src/main/java/frc/robot/commands/utility/WaitCommand.java
@@ -1,5 +1,38 @@
 package frc.robot.commands.utility;
 
-public class WaitCommand {
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj.Timer;
 
+public class WaitCommand extends CommandBase {
+    public Timer timer = new Timer();
+    double time;
+    public WaitCommand (double timeInSeconds, Subsystem... subsystems){
+        for(Subsystem sub: subsystems){
+            addRequirements(sub);
+        }
+        time = timeInSeconds;
+    }
+    @Override
+  public void initialize() {
+    timer.start(); 
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+ 
+}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    double timerRightNow = timer.get();
+    return timerRightNow > time;
+  }
 }
