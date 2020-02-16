371be29cb8fc59b25f079babf5cd700628337542
Scott <scottwork321@gmail.com>
2020-02-16 16:46:49 -0500

    Added method to Set Goal and send to SDashboard

--- src/main/java/frc/robot/commands/drive/RotateToHeadingProfiledCommand.java
+++ src/main/java/frc/robot/commands/drive/RotateToHeadingProfiledCommand.java
@@ -1,6 +1,8 @@
 package frc.robot.commands.drive;
 
 import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SendableRegistry;
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.ProfiledPIDCommand;
 import frc.robot.constants.RobotConst.DriveConst.CharacterizationConst;
@@ -24,11 +26,24 @@ public class RotateToHeadingProfiledCommand extends ProfiledPIDCommand {
         this.s_drive = drive;
         getController().enableContinuousInput(-180, 180);
         getController().setTolerance(PIDConst.DRIVE_TURN_TOLERANCE_DEG, PIDConst.DRIVE_TURN_TOLERANCE_DEG_PER_SECOND);
+
+        SendableRegistry.addLW(getController(), "Rotate to Heading Profiled PID");
     }
 
     @Override
     public boolean isFinished() {
         return getController().atGoal();
+    }
+
+    public void setGoal(double gyroAngle){
+        getController().setGoal(gyroAngle);
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        super.initSendable(builder);
+        builder.setSmartDashboardType("Rotate to Heading Profiled Command");
+
     }
 
 }
