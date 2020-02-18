b73aac3e238313574f8b0c17c3b5571d0302ca4c
Scott <scottwork321@gmail.com>
2020-02-18 15:48:00 -0500

    Added setting speeds to 0 if waiting in MoveBallsToShootCommand

--- src/main/java/frc/robot/commands/intake/MoveBallsToShootCommand.java
+++ src/main/java/frc/robot/commands/intake/MoveBallsToShootCommand.java
@@ -23,13 +23,14 @@ public class MoveBallsToShootCommand extends CommandBase {
                     IntakeConst.UPPER_VERTICAL_SPEED);
         } else {
             // Ball in top position,
-            if(flywheelReady){
+            if (flywheelReady) {
                 // Flywheel ready, shoot balls
                 s_intake.setSpeeds(IntakeConst.ENTRY_SPEED, IntakeConst.CURVE_SPEED, IntakeConst.LOWER_VERTICAL_SPEED,
-                    IntakeConst.UPPER_VERTICAL_SHOOT_SPEED);
+                        IntakeConst.UPPER_VERTICAL_SHOOT_SPEED);
+            } else {
+                s_intake.setSpeeds(0, 0, 0, 0);
             }
         }
-
     }
 
     public void setFlywheelReady(boolean ready) {
