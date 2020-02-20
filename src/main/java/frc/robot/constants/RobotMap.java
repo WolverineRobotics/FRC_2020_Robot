a97cac3dacd80268534b86977ed7b66d338ecc6b
Austin <austinx88@gmail.com>
2020-02-20 16:38:50 -0500

    Updated some CAN ids.

--- src/main/java/frc/robot/constants/RobotMap.java
+++ src/main/java/frc/robot/constants/RobotMap.java
@@ -1,13 +1,17 @@
 package frc.robot.constants;
 
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+
 public class RobotMap{
 
     //TODO: Change addresses
 
-    public static final int DRIVE_LEFT_MOTOR_MASTER_ADDRESS         = 1;
-    public static final int DRIVE_LEFT_MOTOR_SLAVE_ADDRESS          = 2;
-    public static final int DRIVE_RIGHT_MOTOR_MASTER_ADDRESS        = 3;
-    public static final int DRIVE_RIGHT_MOTOR_SLAVE_ADDRESS         = 4;
+    public static final int DRIVE_LEFT_MOTOR_MASTER_ADDRESS         = 10;
+    public static final int DRIVE_LEFT_MOTOR_SLAVE_ADDRESS          = 11;
+
+    public static final int DRIVE_RIGHT_MOTOR_MASTER_ADDRESS        = 15;
+    public static final int DRIVE_RIGHT_MOTOR_SLAVE_ADDRESS         = 16;
 
 
     public static final int DRIVE_LEFT_ENCODER_A                    = 6;
@@ -17,5 +21,7 @@ public class RobotMap{
     public static final int DRIVE_RIGHT_ENCODER_B                   = 5;
 
     public static final int DRIVE_PIGEON_IMU_ADDRESS                = 12;
+
+    public static final SerialPort.Port LIDAR_PORT = Port.kOnboard;
 
 }
