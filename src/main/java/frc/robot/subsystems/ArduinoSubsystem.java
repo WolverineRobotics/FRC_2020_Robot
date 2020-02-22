1d442ffaad8593bc5a9c18c10e5ae7b52bf426b8
Austin <austinx88@gmail.com>
2020-02-22 12:23:30 -0500

    ArduinoSubsystem ready for testing

--- src/main/java/frc/robot/subsystems/ArduinoSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArduinoSubsystem.java
@@ -1,38 +1,35 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.SerialPort;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArduinoSubsystem extends SubsystemBase{
 
-    private SerialPort arduino;
+    private SerialPort serial;
 
-    private Timer timer;
     private int distanceCm;
 
     public ArduinoSubsystem(){
         try {
-            arduino = new SerialPort(9600, Port.kUSB);
+            serial = new SerialPort(115200, Port.kUSB);
             System.out.println("Connected arduino to kUSB");
         } catch (Exception e0) {
             try {
-                arduino = new SerialPort(9600, Port.kUSB1);
+                serial = new SerialPort(115200, Port.kUSB1);
                 System.out.println("Connected arduino to kUSB1");
             } catch (Exception e1) {
                 try {
-                    arduino = new SerialPort(9600, Port.kUSB2);
+                    serial = new SerialPort(115200, Port.kUSB2);
                     System.out.println("Connected arduino to kUSB2");
                 } catch (Exception e2) {
                     System.out.println("Failed all USB connection attempts");
                 }
             }
         }
-        timer = new Timer();
-        timer.start();
         distanceCm = -1;
     }
 
@@ -46,25 +43,20 @@ public class ArduinoSubsystem extends SubsystemBase{
 
     @Override
     public void periodic() {
-        if(arduino.getBytesReceived() > 0) {
-            System.out.println(arduino.readString());
-            //PARSE STRING TO INT
-            int amountBytes = arduino.getBytesReceived();
-            byte[] read = arduino.read(amountBytes);
-            
-            String parse = "";
-            for(byte b : read) {
-                parse += b;
+        if(serial != null) {
+            String read = serial.readString();
+            if(read.length() > 0) {
+                int distance;
+                try {
+                    distance = Integer.parseInt(read);
+                } catch (Exception e) {
+                    System.out.println("Could not parse String: " + read);
+                    return;
+                }
+                distanceCm = distance;
             }
-
-            //PASS TO VARIABLE
-            try {
-                distanceCm = Integer.parseInt(parse);
-            } catch (NumberFormatException e) {
-                System.out.println("Error parsing Arduino bytes: " + read);
-            }
+            updateDashboard();
         }
-        updateDashboard();
     }
 
     @Override
