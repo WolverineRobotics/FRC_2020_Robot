0639280422515e0fe7577022627170c12aed5c3d
Austin <austinx88@gmail.com>
2020-02-27 15:59:45 -0500

    Reorganized DefaultIntake

--- src/main/java/frc/robot/commands/defaultcommands/DefaultIntakeCommand.java
+++ src/main/java/frc/robot/commands/defaultcommands/DefaultIntakeCommand.java
@@ -29,49 +29,7 @@ public class DefaultIntakeCommand extends CommandBase {
 
     @Override
     public void execute() {
-        if (oc.isHoldingLeftTrigger()) { // if is intaking
-            // if(s_intake.isIntakeOpen()) {
-            //     s_intake.setIntakePiston(true);
-            // }
-            // PREVIOUS CODE FOR DEFAULT INTAKE
-            // final boolean[] sen = s_intake.getSensors();
-            // /**
-            //  * The reason why we don't check for sensor 1 sometimes
-            //  * is because the robot can potentially drop the ball @ location entry.
-            //  * So we don't want to disable intake entirely when the ball @ location entry
-            //  * is dropped.
-            //  */
-            // if(!sen[0] && sen[1] && sen[2] && sen[3] && sen[4]) { //if sensor 2, 3, 4, and 5 are detecting but not 1
-            //     s_intake.setEntrySpeed(IntakeConst.ENTRY_SPEED);
-            // } else if(sen[0] && !sen[1] && !sen[2] && !sen[3] && !sen[4]) { //if sensor 1 is only one with ball
-            //     s_intake.setSpeeds(IntakeConst.ENTRY_SPEED, 0, 0, 0);
-            // } else if(sen[1] && !sen[2] && !sen[3] && !sen[4]) { //if sensor 1 (potentially) and 2 is only one with ball
-            //     s_intake.setSpeeds(IntakeConst.ENTRY_SPEED, IntakeConst.CURVE_SPEED, IntakeConst.LOWER_VERTICAL_SPEED, 0);
-            // } else if(sen[1] && sen[2] && !sen[3] && !sen[4]) { //if sensor 1 (potentially), 2, 3, has ball
-            //     s_intake.setSpeeds(IntakeConst.ENTRY_SPEED, IntakeConst.CURVE_SPEED, IntakeConst.LOWER_VERTICAL_SPEED, 0);
-            // } else if(sen[1] && sen[2] && sen[3] && !sen[4]) { //if sensor 1 (potentially), 2, 3, and 4 has ball
-            //     s_intake.setSpeeds(IntakeConst.ENTRY_SPEED, IntakeConst.CURVE_SPEED, IntakeConst.LOWER_VERTICAL_SPEED, IntakeConst.UPPER_VERTICAL_SPEED);
-            // } else if(!sen[0] && !sen[1] && !sen[2] && !sen[3] && !sen[4]) { //if none of the sensors have balls
-            //     s_intake.setEntrySpeed(IntakeConst.ENTRY_SPEED);
-            // }
-            
-            //run intake
-            // s_intake.setMoveBalls(true);
-
-
-        } else if(oc.isHoldingRightTrigger()) { //if operator outaking
-            // boolean[] sen = s_intake.getSensors();
-            // if(!sen[1] && !sen[2] && !sen[4]) {
-            //     s_intake.setSpeeds(0, 0.10, 0.1, 0);
-            // } else if(!sen[2] && !sen[4]) {
-            //     s_intake.setSpeeds(0.3, 0.5, 0, 0);
-            // } else if(!sen[4]) {
-            //     s_intake.setSpeeds(0.3, 0.5, 0.25, 0);
-            // } else if(!sen[4] && !sen[3] && !sen[2]) {
-            //     s_intake.setSpeeds(0.3, 0.5, 0, 0);
-            // } else{
-            //     s_intake.setSpeeds(0.3, 0.5, 0.25, 0.10);
-            // }
+        if(oc.isHoldingRightTrigger()) { // if operator wants to dumby proof intake
             s_intake.setMoveBalls(true);
         } else if(oc.isResetIntakeLogic()) { // if operator wants to auto shoot
             /**
@@ -86,15 +44,15 @@ public class DefaultIntakeCommand extends CommandBase {
             s_intake.currentPossessions.clear();
         } else if(oc.isOutaking()) { //if operator wants to outake the balls from the bottom
             s_intake.setEntrySpeed(-0.3);
-            s_intake.setCurveSpeed(-0.8);
+            s_intake.setCurveSpeed(-0.5);
             s_intake.setVerticalLowerSpeed(-0.5);
             s_intake.setVerticalUpperSpeed(-0.5);
-        } else if(oc.isPOVDown()) { //if operator wants to intake only entry motor
+        } else if(oc.isPOVDown()) { //if operator wants to move balls all to fly wheel
             s_intake.setEntrySpeed(0.1);
             s_intake.setCurveSpeed(0.3);
             s_intake.setVerticalLowerSpeed(0.2);
             s_intake.setVerticalUpperSpeed(0.5);
-        } else if(oc.isPressingB()) {
+        } else if(oc.isPressingB()) { //move front piston
             if(s_intake.isIntakeOpen()) {
                 s_intake.setIntakePiston(false);
             } else {
