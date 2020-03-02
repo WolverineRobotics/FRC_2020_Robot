4cad010329340afef5083d44852502d286a2e17a
Austin <austinx88@gmail.com>
2020-03-02 17:11:12 -0500

    Implemented Climb Encoder reading (through NEO) and piston along with toggleLock method

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -2,10 +2,12 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.fasterxml.jackson.annotation.JacksonInject.Value;
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpiutil.math.MathUtil;
@@ -14,11 +16,15 @@ import frc.robot.constants.RobotMap;
 public class ClimbSubsystem extends SubsystemBase {
 
     private CANSparkMax climb;
+    private CANEncoder encoder;
     private TalonSRX climb_level;
+    private DoubleSolenoid piston;
 
     public ClimbSubsystem() {
         climb = new CANSparkMax(RobotMap.SpeedController.CLIMB, MotorType.kBrushless);
+        encoder = new CANEncoder(climb);
         climb_level = new TalonSRX(RobotMap.SpeedController.CLIMB_LEVEL);
+        piston = new DoubleSolenoid(3, RobotMap.Pneumatic.CLIMB_LOCK_FORWARD, RobotMap.Pneumatic.CLIMB_LOCK_REVERSE);
     }
 
     public void setClimbSpeed(double speed) {
@@ -41,6 +47,18 @@ public class ClimbSubsystem extends SubsystemBase {
         return climb.getOutputCurrent();
     }
 
+    public double getClimbEncoder() {
+        return encoder.getPosition();
+    }
+
+    public void toggleLock() {
+        if(piston.get().equals(DoubleSolenoid.Value.kForward)) {
+            piston.set(DoubleSolenoid.Value.kReverse);
+        } else {
+            piston.set(DoubleSolenoid.Value.kForward);
+        }
+    }
+
     @Override
     public void periodic() {
         updateSDashboard();
@@ -50,6 +68,7 @@ public class ClimbSubsystem extends SubsystemBase {
 
     private void updateSDashboard(){
         SmartDashboard.putNumber("[Climb] Climb Current", getClimbCurrent());
+        SmartDashboard.putNumber("[Climb] Climb Neo Encoder", getClimbEncoder());
     }
 
 }
