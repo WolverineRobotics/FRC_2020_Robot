076b89bf4738fd1d45d0d8e8d69d419dd1f8d454
Austin <austinx88@gmail.com>
2020-03-06 17:04:34 -0500

    Fixed intake clogging

--- src/main/java/frc/robot/commands/defaultcommands/DefaultIntakeCommand.java
+++ src/main/java/frc/robot/commands/defaultcommands/DefaultIntakeCommand.java
@@ -67,17 +67,17 @@ public class DefaultIntakeCommand extends CommandBase {
             s_intake.setEntrySpeed(0.1);
             s_intake.setCurveSpeed(0.15);
             s_intake.setVerticalLowerSpeed(0.25);
-            // s_intake.setVerticalUpperSpeed(1);
+            s_intake.setVerticalUpperSpeed(1);
         } else if(oc.isPressingA()) { //move front piston
             s_intake.toggleIntakePiston();
         } else if(oc.isPressingX()) {
-            List<Ball> mag = s_intake.mag;
-            if(mag.size() > 1) {
-                mag.remove(0);
-                s_intake.moveBallsOneStage();
-            }
+            // List<Ball> mag = s_intake.mag;
+            // if(mag.size() > 1) {
+            //     mag.remove(0);
+            //     s_intake.moveBallsOneStage();
+            // }
         } else if(oc.isIntakeShootTopStageOnly()){
-            s_intake.setVerticalUpperSpeed(0.8);
+            // s_intake.setVerticalUpperSpeed(0.8);
         } else {
             s_intake.setMoveBalls(false);
             s_intake.setSpeeds(0, 0, 0, 0);
