a48d487fe641b6e4d441eab9fe6f8cb9b15f6aa4
Scott <scottwork321@gmail.com>
2020-03-07 11:50:42 -0500

    RightAutoGroup now rotates to vision target

--- src/main/java/frc/robot/commands/groups/RightAutoGroup.java
+++ src/main/java/frc/robot/commands/groups/RightAutoGroup.java
@@ -3,6 +3,8 @@ package frc.robot.commands.groups;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.drive.DriveFowardCommand;
 import frc.robot.commands.drive.RotateToHeadingProfiledCommand;
+import frc.robot.commands.drive.RotateToVisionTargetCommand;
+import frc.robot.subsystems.CameraSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
@@ -12,13 +14,25 @@ public class RightAutoGroup extends SequentialCommandGroup{
     private final DriveSubsystem s_drive;
     private final IntakeSubsystem s_intake;
     private final ShooterSubsystem s_shooter;
+    private final CameraSubsystem s_camera;
 
-    public RightAutoGroup(DriveSubsystem drive, IntakeSubsystem intake, ShooterSubsystem shooter){
+    public RightAutoGroup(DriveSubsystem drive, IntakeSubsystem intake, ShooterSubsystem shooter, CameraSubsystem camera){
         this.s_drive = drive;
         this.s_intake = intake;
         this.s_shooter = shooter;
-        addCommands(new RotateToHeadingProfiledCommand(s_drive, 30),
-        new ShootBallsCommand(s_intake, s_shooter), new RotateToHeadingProfiledCommand(s_drive, 180),
-        new DriveFowardCommand(s_drive, 0.4, 1));
+        this.s_camera = camera;
+        addCommands(
+            new RotateToHeadingProfiledCommand(s_drive, 30),
+
+            new RotateToVisionTargetCommand(s_camera, s_drive){
+                public boolean isFinished(){
+                    return getController().atSetpoint();
+                }
+            },
+
+            new ShootBallsCommand(s_intake, s_shooter), 
+            new RotateToHeadingProfiledCommand(s_drive, 180),
+            new DriveFowardCommand(s_drive, 0.4, 1)
+        );
     }
 }
